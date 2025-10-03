8dff8e0fbbf6e6e3602e5280adde44fdec0cf843
Ayush Gautam <ayushgau194@gmail.com>
2025-10-03 12:10:52 +0530

    added padding

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -6,7 +6,7 @@ const Gallery = () => {
   };
   return (
     <>
-    <div className='grid-rows-4 grid grid-cols-3 gap-2 mb-5 pt-40' style={navStyle}>
+    <div className='grid-rows-4 grid grid-cols-3 gap-2 pb-5 pt-40' style={navStyle}>
       
       <img src="https://res.cloudinary.com/desyzom9w/image/upload/v1710314838/Gallery/rzgxvzww7bejisr4nvk0.jpg" alt="" className='w-full h-auto rounded-lg shadow-md'/>
       <img src="https://res.cloudinary.com/desyzom9w/image/upload/v1710314838/Gallery/rzgxvzww7bejisr4nvk0.jpg" alt="" className='w-full h-auto rounded-lg shadow-md '/>
